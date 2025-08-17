63a0e9214fb58e78b6d5951197e95ac81cfd68a9
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-08-17 13:30:38 -0700

    setup basic global and impl kt class for @AlsoSylv

---
+++ library/src/main/kotlin/one/devos/nautical/starmedia/NativeGSMTC.kt
@@ -0,0 +1,5 @@
+package one.devos.nautical.starmedia
+
+internal class NativeGSMTC {
+
+}
--- library/src/main/kotlin/one/devos/nautical/starmedia/StarMediaLib.kt
+++ library/src/main/kotlin/one/devos/nautical/starmedia/StarMediaLib.kt
@@ -1,3 +1,12 @@
-fun main() {
+package one.devos.nautical.starmedia
 
+class StarMediaLib {
+    external fun requestManager(): Long
+    external fun tryPause(gsmtcs: Long)
+    external fun metadata(gsmtcs: Long): Array<String?>?
+    external fun setPropertyChangedCallback(obj: Any?, gsmtcs: Long)
+
+    init {
+        System.loadLibrary("starmedia_natives")
+    }
 }
