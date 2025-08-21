2fb6b2b6a1fafa7561f00521d9b06f062c994c77
Naz <bluspringindohq@gmail.com>
2025-08-21 11:54:14 +0800

    Fixed broken printing on J21+

--- library/src/test/kotlin/one/devos/nautical/starmedia/TestClient.kt
+++ library/src/test/kotlin/one/devos/nautical/starmedia/TestClient.kt
@@ -1,5 +1,7 @@
 package one.devos.nautical.starmedia
 
+import java.io.OutputStreamWriter
+import java.io.PrintWriter
 import java.lang.Thread.sleep
 
 fun main(args: Array<String>) {
@@ -12,19 +14,20 @@ fun main(args: Array<String>) {
 
         sleep(1000)
 
-        println("GSMTC Long ID: $gsmtc")
+        val console = PrintWriter(OutputStreamWriter(System.out, Charsets.UTF_8), true)
+
+        console.println("GSMTC Long ID: $gsmtc")
         // print out the metadata array
         // uh oh why is it not going through UTF-8? uhhh
 //        println(metadata?.joinToString("\n")) [this works but im just commenting it out bc of testing below]
 
-
-        println(metadata?.get(0)) // title?
-        println(metadata?.get(1)) // this should be artist but uh
-        println(metadata?.get(2)) // subtitle?
-        println(metadata?.get(3)) // album name [doesnt seem to be passed through by apple music at least] =/
-        println(metadata?.get(4)) // album artist?
-        println(metadata?.get(5)) // album length [just gives 0 rn?
-        println(metadata?.get(6)) // track number?
+        console.println(metadata?.get(0)) // title?
+        console.println(metadata?.get(1)) // this should be artist but uh
+        console.println(metadata?.get(2)) // subtitle?
+        console.println(metadata?.get(3)) // album name [doesnt seem to be passed through by apple music at least] =/
+        console.println(metadata?.get(4)) // album artist?
+        console.println(metadata?.get(5)) // album length [just gives 0 rn?
+        console.println(metadata?.get(6)) // track number?
 
         // tries to pause music every second bc of the sleep lmao
 //        StarMediaLib().tryPause(gsmtc)
